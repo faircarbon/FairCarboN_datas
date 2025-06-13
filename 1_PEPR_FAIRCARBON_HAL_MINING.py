ccd5cd80ebd6c7b1adda061df34016f3f6f0fd23
Jérôme Dutroncy <jerome.dutroncy@univ-grenoble-alpes.fr>
2025-06-13 14:38:17 +0200

    Création script Mining de HAL pour FairCarboN + comparaison in/out collection FairCarboN

---
+++ 1_PEPR_FAIRCARBON_HAL_MINING.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+import plotly.express as px
+from wordcloud import WordCloud
+import plotly.graph_objects as go
+from Publications import afficher_publications_hal
+import datetime
+
+
+###############################################################################################
+########### TITRE DE L'ONGLET #################################################################
+###############################################################################################
+st.set_page_config(
+    page_title="FAIRCARBON HAL DATA MINING",
+    page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "développé par Jérôme Dutroncy"}
+)
+
+######################################################################################################################
+########### CHOIX VISUELS ############################################################################################
+######################################################################################################################
+# taille et couleurs des sous-titres
+couleur_subtitles = (250,150,150)
+taille_subtitles = "25px"
+couleur_subsubtitles = (60,150,160)
+taille_subsubtitles = "25px"
+
+###############################################################################################
+########### REQUETES HAL ######################################################################
+###############################################################################################
+
+st.title(":grey[Extraction des publications sur HAL]")
+
+start_year=2023
+end_year=2025
+#Liste_chercheurs = ["jean-philippe jenny","jerome demarty"]
+Liste_chercheurs = ["jerome demarty","jean-philippe jenny"]
+
+with st.spinner("Recherche en cours"):
+    liste_columns_hal = ['Store','Auteur_recherché','Ids','Titre et auteurs','Uri','Type','Type de document', 'Date de production','Collection','Collection_code','Auteur']
+    df_global_hal = pd.DataFrame(columns=liste_columns_hal)
+    for i, s in enumerate(Liste_chercheurs):
+        url_type = f'http://api.archives-ouvertes.fr/search/?q=text:"{s.lower().strip()}"&rows=1500&wt=json&fq=producedDateY_i:[{start_year} TO {end_year}]&sort=docid asc&fl=docid,label_s,uri_s,submitType_s,docType_s, producedDateY_i,authLastNameFirstName_s,collName_s,collCode_s'
+        df = afficher_publications_hal(url_type, s)
+        dfi = pd.concat([df_global_hal,df], axis=0)
+        dfi.reset_index(inplace=True)
+        dfi.drop(columns='index', inplace=True)
+        df_global_hal = dfi
+    df_global_hal.sort_values(by='Ids', inplace=True, ascending=False)
+    df_global_hal.reset_index(inplace=True)
+    df_global_hal.drop(columns='index', inplace=True)
+
+
+filtered_df = df_global_hal[df_global_hal['Collection_code'].apply(lambda names: 'FAIRCARBON' in names)]
+
+st.metric(label="Nombre de publications globales", value=len(df_global_hal))
+
+st.metric(label="Nombre de publications dans la collection FairCarboN", value=len(filtered_df))
+
+st.dataframe(filtered_df)
---
+++ Publications.py
@@ -0,0 +1,70 @@
+import requests
+import json.decoder
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+
+
+def afficher_texte_reponse_api_hal(requete_api_hal: str):
+    """Interroge l'API HAL et affiche le texte de la réponse
+    Paramètre = requête API HAL"""
+    reponse = requests.get(requete_api_hal, timeout=5)
+    print(reponse.text)
+
+
+def afficher_erreur_api(erreur):
+    """Affiche les erreurs soulevées lors de l'interrogation de l'API HAL
+    Paramètre = erreur"""
+    print(f"Les résultats HAL n'ont pas pu être récupérés ({erreur}).")
+
+
+def afficher_publications_hal(requete_api_hal: str, auteur):
+    """Interroge l'API HAL et affiche les infos des documents de la réponse
+    Paramètre = requête API HAL avec wt=json (str)"""
+    try:
+        reponse = requests.get(requete_api_hal, timeout=5)
+        ids = []
+        labels = []
+        uris = []
+        types = []
+        docTypes = []
+        date = []
+        source = []
+        author = []
+        collection = []
+        collection_code = []
+        for doc in reponse.json()['response']['docs']:
+            ids.append(int(doc['docid']))
+            labels.append(soup(doc['label_s'], 'html.parser').text)
+            uris.append(doc['uri_s'])
+            types.append(doc['submitType_s'])
+            docTypes.append(doc['docType_s'])
+            date.append(doc['producedDateY_i'])
+            author.append(doc['authLastNameFirstName_s'])#authIdHal_i
+            collection.append(doc['collName_s'])
+            collection_code.append(doc['collCode_s'])
+            source.append('HAL')
+
+        reponse_df = pd.DataFrame({'Store':source,
+                                   'Auteur_recherché':auteur,
+                                   'Ids':ids,
+                                   'Titre et auteurs':labels,
+                                   'Uri':uris,
+                                   'Type':types,
+                                   'Type de document':docTypes,
+                                   'Date de production':date,
+                                   'Collection':collection,
+                                   'Collection_code':collection_code,
+                                   'Auteur':author})
+
+    except requests.exceptions.HTTPError as errh:
+        afficher_erreur_api(errh)
+    except requests.exceptions.ConnectionError as errc:
+        afficher_erreur_api(errc)
+    except requests.exceptions.Timeout as errt:
+        afficher_erreur_api(errt)
+    except requests.exceptions.RequestException as err:
+        afficher_erreur_api(err)
+    except json.decoder.JSONDecodeError as errj:
+        afficher_erreur_api(errj)
+
+    return reponse_df
