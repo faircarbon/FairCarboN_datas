f616b17e33ebb67e67a0e4a55c9893dcd37eea1f
Jérôme Dutroncy <jerome.dutroncy@univ-grenoble-alpes.fr>
2026-03-17 17:22:00 +0100

    Sunburst key numbers

--- pages/2_Key_Numbers.py
+++ pages/2_Key_Numbers.py
@@ -38,12 +38,42 @@ def read_data(path):
 ########### PARAMETRES ###############################################################################################
 ######################################################################################################################
 
+couleurs = {"ALAMOD":"#1f77b4",
+              "SLAM-B":"#ff7f0e",
+              "RIFT":"#2ca02c",
+              "CrosyeN":"#d62728",
+              "CarboNium":"#9467bd",
+              "CABESTAN":"#8c564b",
+              "CANETE":"#e377c2",
+              "DEEP-C":"#7f7f7f",
+              "Drought for C":"#bcbd22",
+              "PEACE":"#17becf",
+              "TROPECOS":"#393b79",
+              "CLIM-FAS":"#637939",
+              "CO2_CMPhi":"#8c6d31",
+              "GREENSCALE":"#843c39",
+              "PREFALIM":"#7b4173",
+              "RhizoSeqC":"#3182bd",
+              "Gouvernance":"#CCCCFF",
+              "Labo":"#020242",
+              "Site":"#AC1B08",
+              "DIR":"#313695",
+              "CR":"#4575b4",
+              "INGE":"#74add1",
+              "DOC":"#a6cee3",
+              "POSTDOC":"#fdae61",
+              "PROFESSEUR":"#f46d43",
+              "MAITRE_DE_CONF":"#d73027",
+              "ASSIT_INGE":"#a50026"}
 
+size_sunburst = 600
 
 ######################################################################################################################
 ########### DONNEES ##################################################################################################
 ######################################################################################################################
-data = read_data("Data/FairCarboN_Datas_Contacts")
+Contacts = read_data("Data/FairCarboN_Datas_Contacts")
+Budgets = read_data("Data/Budgets_PEPR_FairCarboN")
+Labo = read_data("Data/FairCarboN_Datas_Labo3")
 
 ######################################################################################################################
 ########### AFFICHAGE ################################################################################################
@@ -73,29 +103,95 @@ with col3:
     st.metric(value=150, label="", label_visibility="hidden")
 
 
-st.title("Chiffres clés - Key Numbers || Par projet - By Project")
+st.title("Chiffres clés - Key Numbers || Par projet - By project")
+
+# Comptage par projet et statut
+Contacts_selec = Contacts[["PEPR FairCarboN", "projet", "Fonction"]]
+Contacts_ag = Contacts.groupby(["projet", "Fonction"]).size().unstack(fill_value=0)
+Contacts_long = Contacts_ag.reset_index().melt(
+    id_vars="projet",
+    var_name="Fonction",
+    value_name="compte"
+)
+# On enlève les lignes où count = 0 (sinon ça crée des branches vides)
+Contacts_long = Contacts_long[Contacts_long["compte"] > 0]
+Contacts_long["PEPR FairCarboN"]="FairCarboN"
 
 
-import pandas as pd
-import plotly.express as px
-
-# --- Création d'un DataFrame exemple ---
-data = {
-    "Continent": ["FC", "FC", "FC","FC", "FC", "FC","FC", "FC", "FC","FC", "FC", "FC","FC", "FC"],
-    "Pays": ["P1", "P1", "P1", "P1", "P1", "P1","P1", "P2", "P2", "P2", "P2", "P2", "P2", "P2" ],
-    "Ville": ["CR", "DIR", "DOC", "POSTDOC", "INGE", "TECH","Autres", "CR", "DIR", "DOC", "POSTDOC", "INGE", "TECH","Autres"],
-    "Population": [15, 10 , 3, 6 , 5, 12, 0, 18, 20 , 13, 0 , 5, 2, 4]
-}
-
-df = pd.DataFrame(data)
-
-# --- Création du graphique Sunburst ---
-fig = px.sunburst(
-    df,
-    path=["Continent", "Pays", "Ville"],  # hiérarchie
-    values="Population",                 # taille des segments
-    color="Continent",                   # coloration par continent
-    title="Répartition de population par continent, pays et ville"
+Labo_selec = Labo[["PEPR FairCarboN", "projet", "Type_Data"]]
+Labo_ag = Labo.groupby(["projet", "Type_Data"]).size().unstack(fill_value=0)
+Labo_long = Labo_ag.reset_index().melt(
+    id_vars="projet",
+    var_name="Type_Data",
+    value_name="compte"
+)
+# On enlève les lignes où count = 0 (sinon ça crée des branches vides)
+Labo_long["compte_affiche"] = Labo_long["compte"].replace(0, 1)
+Labo_long = Labo_long[Labo_long["compte_affiche"] > 0]
+Labo_long["PEPR FairCarboN"]="FairCarboN"
+Labo_long["label"] = Labo_long.apply(
+    lambda r: f"{r["Type_Data"]} (0)" if r["compte"] == 0 else f"{r['compte']}",
+    axis=1
 )
 
-st.plotly_chart(fig, use_container_width=True)
+fig1 = px.sunburst(
+    Budgets,
+    path=["PEPR FairCarboN", "Projet", "Budget"],  
+    values="Budget",                 
+    color="Projet",
+    color_discrete_map=couleurs
+)
+
+fig1.update_layout(
+    width=size_sunburst,   
+    height=size_sunburst    
+)
+fig1.update_traces(
+    insidetextorientation='radial',
+    marker=dict(
+        line=dict(color='white', width=2)
+    ),
+    root_color="white"   
+)
+
+fig2 = px.sunburst(
+    Contacts_long,
+    path=["PEPR FairCarboN", "projet", "Fonction","compte"],  
+    values="compte",                 
+    color="Fonction",
+    color_discrete_map=couleurs
+)
+
+fig2.update_layout(
+    width=size_sunburst,   
+    height=size_sunburst    
+)
+
+fig2.update_traces(
+    insidetextorientation='radial',
+    marker=dict(
+        line=dict(color='white', width=2)
+    ),
+    root_color="white"   
+)
+
+fig3 = px.sunburst(
+    Labo_long,
+    path=["PEPR FairCarboN", "projet", "Type_Data","label"],  
+    values="compte_affiche",                 
+    color="Type_Data",
+    color_discrete_map=couleurs
+)
+
+fig3.update_layout(
+    width=size_sunburst,   
+    height=size_sunburst    
+)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.plotly_chart(fig1, use_container_width=True)
+with col2:
+    st.plotly_chart(fig2, use_container_width=True)
+with col3:
+    st.plotly_chart(fig3, use_container_width=True)
